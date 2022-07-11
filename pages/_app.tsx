6ec96d3239e32917c88276a4ca4aac9540417154
berkaybaltaci <berkaybaltaci@gmail.com>
2022-07-11 19:40:32 +0300

    Switched the logic to query the mongo database with graphql query.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { HeaderResponsive } from '../components/ui/header';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '../lib/apollo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const links = [
@@ -8,10 +10,10 @@ function MyApp({ Component, pageProps }: AppProps) {
     { link: '/products', label: 'Products' },
   ];
   return (
-    <>
+    <ApolloProvider client={apolloClient}>
       <HeaderResponsive links={links} />
       <Component {...pageProps} />
-    </>
+    </ApolloProvider>
   );
 }
 
